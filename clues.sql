14ddea9bd5227d2bf757ac41b0216a63ff6d94f6
candylxy95 <lee.ydnac@gmail.com>
2024-11-27 17:23:52 +0800

    redo nested query

--- clues.sql
+++ clues.sql
@@ -3,16 +3,13 @@
 SELECT name, region, population FROM countries WHERE region = 'Southern Europe' ORDER BY population ASC LIMIT 1;
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
-
 -- Write SQL query here
 SELECT code FROM countries WHERE name = 'Holy See (Vatican City State)';
 SELECT countrycode, language FROM countrylanguages WHERE countrycode = 'VAT';
-
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
 -- Write SQL query here
-SELECT countrycode FROM countrylanguages WHERE language = 'Italian' AND countrycode NOT IN (SELECT countrycode FROM countrylanguages WHERE language != 'Italian');
-
+SELECT countrycode FROM countrylanguages GROUP BY countrycode HAVING COUNT(countrycode) = 1 AND MAX(language) = 'Italian';
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 -- Write SQL query here
@@ -22,10 +19,9 @@ SELECT name FROM cities WHERE countrycode = 'SMR';
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
-SELECT countrycode FROM cities WHERE name ~ 'Serra' AND countrycode IN (SELECT countrycode FROM countries WHERE region = 'South America');
+SELECT cities.countrycode FROM cities JOIN countries ON cities.countrycode = countries.code WHERE cities.name ~ 'Serra' AND countries.region = 'South America';
 
-
--- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
+-- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towardsq
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
